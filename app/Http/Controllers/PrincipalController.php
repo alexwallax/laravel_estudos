e391d7443e5ef21cb101f3dc34e15bd39a5acdaf
Alex Wallace <alexwallax@gmail.com>
2022-01-29 10:53:34 -0300

    views principal, sobre-nos e contato criadas

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        echo 'Pagína princiapl!!!';
+        return view('site.principal');
     }
 }

108c2ae4bef32b166b70ab900de541d172caab29
Alex Wallace <alexwallax@gmail.com>
2022-02-13 15:58:41 -0300

    for no blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -38,44 +38,59 @@
 
 {{-- @isset verifica se a variavel foi definida  --}}
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[2]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[2]['status'] }}
-    <br>
 
-    {{--isset testa se o indice cnpj existe, se não existir ele não entra no codigo--}}
-    {{--
-    @isset($fornecedores[0]['cnpj']) 
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
 
-    CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    @empty($fornecedores[0]['cnpj'])
-        - Vazio
-    @endempty
-    @endisset
-    --}}
+        {{--isset testa se o indice cnpj existe, se não existir ele não entra no codigo--}}
+        {{--
+        @isset($fornecedores[0]['cnpj']) 
 
-    CNPJ: {{ $fornecedores[2]['cnpj'] ?? 'Dado não foi preenchido' }}
-    <br>
-    <!--
-        $variável testada não estiver definida
-        ou 
-        $variável testada possuir o valor null
-        vai colocar um valor padrão no caso -> 'Dado não foi preenchido'
-    -->
-    Telefone: ({{ $fornecedores[2]['ddd'] ?? '' }}) {{ $fornecedores[2]['telefone'] ?? '' }}
-    @switch($fornecedores[2]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fotaleza - CE
-            @break
-        @default
-            Estado não indentificado
-    @endswitch
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+        @endisset
+        --}}
 
-@endif
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'Dado não foi preenchido' }}
+        <br>
+        <!--
+            $variável testada não estiver definida
+            ou 
+            $variável testada possuir o valor null
+            vai colocar um valor padrão no caso -> 'Dado não foi preenchido'
+        -->
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <hr>
+    
+        {{--
+        @switch($fornecedores[2]['ddd'])
+            @case('11')
+                São Paulo - SP
+                @break
+            @case('32')
+                Juiz de Fora - MG
+                @break
+            @case('85')
+                Fotaleza - CE
+                @break
+            @default
+                Estado não indentificado
+        @endswitch
+        --}}
 
+    @endfor
+
+@endisset
+
+<br>
+
+{{--
+@for($i = 0; $i < 10; $i++)
+    {{ $i }} <br>
+@endfor
+--}}
